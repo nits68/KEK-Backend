ed0bb641fe6c082b950e7ee40c6a08105c2ac4a3
nits68 <nits.laszlo@jedlik.eu>
2024-12-23 21:07:22 +0100

    corret order controller

--- src/order/orderDetail.schema.ts
+++ src/order/orderDetail.schema.ts
@@ -10,21 +10,10 @@ const orderDetailSchema = new Schema<IOrderDetail>({
         ref: "Offer",
         required: true,
     },
-    product_id: {
-        type: Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-    },
     quantity: {
         type: Number,
         min: 0,
         required: true,
-    },
-    stars: {
-        type: Number,
-        min: 0,
-        max: 5,
-        default: 0,
     },
 });
 
