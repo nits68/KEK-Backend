e85ea2ff22d3e86bddb9d66a33a282dace0b8c30
nits68 <nits.laszlo@jedlik.eu>
2024-12-27 01:22:15 +0100

    delete import-export bat-s

--- data/export_users_from_atlas.bat
+++
@@ -1,1 +0,0 @@
-"c:\Program Files\MongoDB\Server\7.0\bin\mongoexport.exe" --uri="mongodb+srv://m001-student:m001-student@sandbox.3fiqf.mongodb.net/KEK_DB" --collection=users --out=users_new.json --jsonArray
